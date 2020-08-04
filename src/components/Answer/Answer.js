643693892abd602390a97f38bf3986af70a04b2f
48lika48 <48lika48@mail.ru>
2020-08-04 18:24:18 +0300

    feat: add static information

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './Answer.css';
-// import logo from '../assets/logo.svg';
+import bird from '../../assets/bird.jpg';
 
 class Answer extends React.Component {
   render() {
     return (
       <div className='answer'>
-        <div className='choice'></div>
-        <div className='information'></div>
+        <div className='choice'>
+          <div className='example'><span className='dots'>•</span>Name</div>
+          <div className='example'><span className='dots'>•</span>Name</div>
+          <div className='example'><span className='dots'>•</span>Name</div>
+          <div className='example'><span className='dots'>•</span>Name</div>
+          <div className='example'><span className='dots'>•</span>Name</div>
+          <div className='example'><span className='dots'>•</span>Name</div>
+        </div>
+        <div className='information'>
+          <div className='information-wrapper'>
+            <div className='answer-picture'>
+              <img className='answer-bird' src={bird} alt='bird'/>
+            </div>
+            <div className='answer-voice'>
+              <div className='answer-name'>Name</div>
+              <div className='latin-name'>Latin Name</div>
+              <div className='answer-sound'>Sound</div>
+            </div>
+          </div>
+          <div className='answer-about'>Ворон – крупная птица.</div>
+        </div>
       </div>
     );
   }
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Question.css';
-// import logo from '../assets/logo.svg';
+import bird from '../../assets/bird.jpg';
 
 class Question extends React.Component {
   render() {
     return (
       <div className='question'>
-        
+        <div className='question-picture'>
+          <img className='question-bird' src={bird} alt='bird'/>
+        </div>
+        <div className='question-voice'>
+          <div className='question-name'>Name</div>
+          <div className='question-sound'>Sound</div>
+        </div>
       </div>
     );
   }
